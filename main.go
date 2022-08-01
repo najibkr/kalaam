a87777703a39451d116a42a1719c6f01402135b7
Najibullah Khoda Rahim <parwandaha@gmail.com>
2022-08-02 02:16:10 +0430

    Modified more stuff

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Hello %s! This is the Zabaan programming language!\n",
+	fmt.Printf("Hello %s! This is the Kalaam programming language!\n",
 		user.Username)
 	fmt.Printf("Feel free to type in commands\n")
 	repl.Start(user.Username, os.Stdin, os.Stdout)
